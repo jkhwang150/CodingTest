5959fcef720e61f55a2456754e673d24f2963bdd
jkhwang150 <75780140+jkhwang150@users.noreply.github.com>
2023-09-16 13:31:44 +0900

    Create 숫자 문자열과 영단어.py

---
+++ python/숫자 문자열과 영단어.py
@@ -0,0 +1,8 @@
+def solution(s):
+    
+    num_str = ['zero','one','two','three','four','five','six','seven','eight','nine']
+    for i in range(len(num_str)):
+        s = s.replace(num_str[i],str(i))
+        
+    
+    return int(s)
