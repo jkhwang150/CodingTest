f396429ed327560ca48499083837ec87796f8eaf
jkhwang150 <75780140+jkhwang150@users.noreply.github.com>
2024-02-06 11:30:42 +0900

    정확성, 효율성 통과

--- python/해쉬/완주하지 못한 선수.py
+++ python/해쉬/완주하지 못한 선수.py
@@ -5,3 +5,18 @@ def solution(participant, completion):
             participant.remove(i)
     answer = participant[0]
     return answer
+
+# 효율성 정확성 통과
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+    answer = ''
+    for i in range(0,len(completion)):
+        if participant[i] == completion[i]:
+            continue
+        else:
+            answer = participant[i]
+            break
+    if answer == '':
+        answer = participant[-1]
+    return answer
