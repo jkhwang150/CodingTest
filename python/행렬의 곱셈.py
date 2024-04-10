bce4c8b864b09d418a74db1200fd696db3755bb9
jkhwang150 <75780140+jkhwang150@users.noreply.github.com>
2024-04-10 10:37:10 +0900

    Create 행렬의 곱셈.py

---
+++ python/행렬의 곱셈.py
@@ -0,0 +1,16 @@
+def solution(arr1, arr2):
+    answer = []
+    m, n, r = len(arr1), len(arr1[0]), len(arr2[0])
+    # arr1의 한행씩 계산
+    for i in range(m):
+        arr = arr1[i]
+        result = []
+        # arr2의 한행씩 계산
+        for j in range(r):
+            hap = 0
+            for k in range(n):
+                hap += arr[k] * arr2[k][j]
+            result.append(hap)
+        answer.append(result)
+
+    return answer
